deabc16322512261d223629851a07d489965e71d
sergei <mikhailovse@mail.ru>
2018-03-18 23:39:07 +0300

    add unit tests

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
-#include "test.h"
+/*#include <iostream>
+#include "CFactoryInfantry.h"
+#include "CInfantry.h"
+#include "CFactoryWorkers.h"
+#include "CWorkers.h"
+#include "CWarrior.h"
+#include "EnemyHeart.h"
+#include "Units.h"
 
-int main(int argc, char **argv) {
+int main()
+{
+	CInfantry *UserInf = CFactoryInfantry::create(userwar);
+	std::cout<<UserInf->getArmor();
+	CInfantry *ComputerInf = CFactoryInfantry::create(computerwar);
+	std::cout<<UserInf->getDamage();
+	delete UserInf;
+	delete ComputerInf;
+	return 0;
+}*/
+#include "test.h"
+int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
--- test.h
+++ test.h
@@ -10,11 +10,54 @@
 #include "EnemyHeart.h"
 #include "Units.h"
 
-TEST(test_FInfantry, test1) {
+TEST(test_FInfantryhealth, test1) {
 	CInfantry *UserInf = CFactoryInfantry::create(userwar);
 	EXPECT_EQ(UserInf->getHealth(), 100);
 	CInfantry *ComputerInf = CFactoryInfantry::create(computerwar);
-	EXPECT_EQ(UserInf->getHealth(), 120);
+	EXPECT_EQ(ComputerInf->getHealth(), 120);
+	delete UserInf;
+	delete ComputerInf;
 }
+TEST(test_FInfantrydamage, test1) {
+	CInfantry *UserInf = CFactoryInfantry::create(userwar);
+	EXPECT_EQ(UserInf->getDamage(), 40);
+	CInfantry *ComputerInf = CFactoryInfantry::create(computerwar);
+	EXPECT_EQ(UserInf->getDamage(), 40);
+	delete UserInf;
+	delete ComputerInf;
+}
+TEST(test_FInfantryarmor, test1) {
+	CInfantry *UserInf = CFactoryInfantry::create(userwar);
+	EXPECT_EQ(UserInf->getArmor(), 5);
+	CInfantry *ComputerInf = CFactoryInfantry::create(computerwar);
+	EXPECT_EQ(UserInf->getArmor(), 5);
+	delete UserInf;
+	delete ComputerInf;
+}
+TEST(test_FWorkers, test1) {
+	CWorkers *Work = CFactoryWorkers::create();
+	EXPECT_EQ(Work->getHealth(), 30);
+	delete Work;
+}
+TEST(test_EnemyHeartDMG, test1) {
+	EnemyHeart& A = EnemyHeart::getInstance();
+	EXPECT_EQ(A.HeartDmg, 50);
+}
+TEST(test_EnemyHeartARM, test1) {
+	EnemyHeart& A = EnemyHeart::getInstance();
+	EXPECT_EQ(A.HeartArmor, 10);
+}
+TEST(test_EnemyHeartHP, test1) {
+	EnemyHeart& A = EnemyHeart::getInstance();
+	EXPECT_EQ(A.HeartHP, 250);
+}
+TEST(test_EnemyHeart2, test1) {// Проверка того, что элемент синглтона единственнен
+	EnemyHeart& A = EnemyHeart::getInstance();
+	A.HeartHP = 100;
+	EnemyHeart& B = EnemyHeart::getInstance();
+	EXPECT_EQ(B.HeartHP, 100);
+}
+
+
 
 #endif
