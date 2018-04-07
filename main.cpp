9146560c380ffc1c2bb14c345afb544562cbc3cc
sergei <mikhailovse@mail.ru>
2018-04-07 04:44:40 +0300

    fix CMakeLists

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
+#include <iostream>
 
-#include "test.h"
-int main(int argc, char *argv[]) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main()
+{
+  std::cout << "Hello, it is a console game\n";
+  return 0;
 }
